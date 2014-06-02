266137a69a7d7eeaaa747e9c6077003f45dd10e5
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-06-03 06:19:00 +0900

    Abolished create catkin workspace

--- ros_setup.sh
+++ ros_setup.sh
@@ -13,13 +13,5 @@ source ~/.bashrc
 source /opt/ros/hydro/setup.bash
 sudo apt-get install python-rosinstall
 
-#Create a catkin workspace
-source /opt/ros/hydro/setup.bash
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-catkin_init_workspace
-cd ~/catkin_ws
-catkin_make
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-
 sudo aptitude install ros-hydro-moveit-full python-wstool
+
