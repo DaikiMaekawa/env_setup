d0a48dbaa2757bd94b1a709613b19910abb75cb3
Daiki Maekawa <method_aspect_card@yahoo.co.jp>
2014-12-13 02:11:23 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ desktop_capture="kazam"
 pdf_viewer="imagemagick"
 video_editor="kdenlive"
 
-sudo apt-get update
-sudo apt-get install aptitude
-sudo aptitude install nkf git screen vim rubygems1.9.1 ruby1.9.1-dev ipython g++ trash-cli ${pdf_viewer} ${image_viewer} ${image_editor} ${desktop_capture} ${video_editor}
+sudo apt-get update -qq
+sudo apt-get install -y aptitude
+sudo aptitude install -y nkf git screen vim rubygems1.9.1 ruby1.9.1-dev ipython g++ trash-cli ${pdf_viewer} ${image_viewer} ${image_editor} ${desktop_capture} ${video_editor}
 #./ros_setup.sh
 
 #link dotfiles
@@ -31,6 +31,6 @@ LANG=c xdg-user-dirs-gtk-update
 bit=$(uname -m)
 
 #Install Dropbox
-wget -O - "https://www.dropbox.com/download?plat=lnx.${bit}" | tar xzf -
-.dropbox-dist/dropboxd
+#wget -O - "https://www.dropbox.com/download?plat=lnx.${bit}" | tar xzf -
+#.dropbox-dist/dropboxd
 
