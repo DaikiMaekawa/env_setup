b937630181cc05dde6780f70f8f27abcbaddba25
DaikiMaekawa <d.maekawa@rapyuta-robotics.com>
2016-04-05 19:24:28 +0900

    check file already exist or not

--- install_recipe.rb
+++ install_recipe.rb
@@ -1,3 +1,5 @@
+home_dir = "/home/" + node[:current_user]
+
 case node[:platform]
 when "debian", "ubuntu"
     package "lldb-3.6" do
@@ -13,6 +15,7 @@ else
 end
 
 # link to dotfiles
-link "/home/" + node[:current_user] do 
+link home_dir do 
     to "daiki-env-setup/dotfiles/.vimrc"
+    not_if 'test -e %s' % home_dir + "/.vimrc"
 end
