e0d32f4503f9cc8c80a4100cd27ae3aa566d49a8
Daiki Maekawa <d.maekawa@rapyuta-robotics.com>
2016-05-12 16:11:11 +0900

    neobundle error fixed

--- install_recipe.rb
+++ install_recipe.rb
@@ -1,5 +1,8 @@
 HOME_DIR = Dir.home
 CURRENT_SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
+#CURRENT_USER_NAME = `hostname`.chop
+CURRENT_USER_NAME = "daikimaekawa"
+puts "CURRENT_USER_NAME = " + CURRENT_USER_NAME
 
 case node[:platform]
 when "debian", "ubuntu"
@@ -51,8 +54,17 @@ link HOME_DIR do
     not_if 'test -e %s' % HOME_DIR + "/.ycm_extra_conf_ros.py"
 end
 
+directory HOME_DIR + "/.vim" do
+    mode "775"
+    owner CURRENT_USER_NAME
+    group CURRENT_USER_NAME
+    not_if 'test -e %s' % HOME_DIR + "/.vim"
+end
+
 directory HOME_DIR + "/.vim/colors" do
     mode "775"
+    owner CURRENT_USER_NAME
+    group CURRENT_USER_NAME
     not_if 'test -e %s' % HOME_DIR + "/.vim/colors"
 end
 
@@ -63,11 +75,11 @@ end
 
 # install neobundle
 
-directory HOME_DIR + "/.vim/bundle" do
-    mode "775"
-    not_if 'test -e %s' % HOME_DIR + "/.vim/bundle"
+execute "download install.sh" do
+    user CURRENT_USER_NAME
+    command "curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh"
 end
 
-git HOME_DIR + "/.vim/bundle/neobundle.vim" do
-    repository "git://github.com/Shougo/neobundle.vim"
+execute "sh ./install.sh" do
+    user CURRENT_USER_NAME
 end
