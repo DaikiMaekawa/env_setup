7d83222dc9095949cefc39af54a585c4f3da824b
DaikiMaekawa <d.maekawa@rapyuta-robotics.com>
2016-10-07 21:38:53 -0800

    use c++11 option instead from 03

--- dotfiles/.ycm_extra_conf_ros.py
+++ dotfiles/.ycm_extra_conf_ros.py
@@ -73,8 +73,8 @@ default_flags = [
     # For a C project, you would set this to something like 'c99' instead of
     #'-stdlib=libc++',
     # 'c++11',
-    '-std=c++03',
-    #'-std=c++11',
+    #'-std=c++03',
+    '-std=c++11',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c++ headers.
