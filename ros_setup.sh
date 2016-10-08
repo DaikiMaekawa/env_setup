cb036848f8b230a91ef6ed2ed71159869ccd9e58
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2016-10-07 21:38:53 -0800

    create a workspace

--- ros_setup.sh
+++ ros_setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#Ubuntu 12.04, hydro
+#Install hydro to Ubuntu 12.04
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
-
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-hydro-desktop-full
@@ -13,3 +12,12 @@ echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 source /opt/ros/hydro/setup.bash
 sudo apt-get install python-rosinstall
+
+#Create a catkin workspace
+source /opt/ros/hydro/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
