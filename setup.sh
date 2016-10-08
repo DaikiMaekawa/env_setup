3a13c5efd538ca025844355020e41a23a03d8400
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2016-10-07 21:38:53 -0800

    Added git config

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ set -e
 #base
 sudo apt-get install aptitude
 sudo aptitude install git screen vim
-#./ros_setup.sh
+./ros_setup.sh
 
 #link dotfiles
 ln -sf $PWD/dotfiles/.vimrc ~/.vimrc
 ln -sf $PWD/dotfiles/.screenrc ~/.screenrc
+
+#git setup
+git config --global user.name "DaikiMaekawa"
+git config --global user.email "method_aspect_card@yahoo.co.jp"
