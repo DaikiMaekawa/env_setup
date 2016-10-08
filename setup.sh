2b258ae422cfc0cf1f40ad2198377c97346920a7
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2016-10-07 21:38:53 -0800

    Install jekyll

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 #base
 sudo apt-get install aptitude
-sudo aptitude install git screen vim
+sudo aptitude install git screen vim rubygems
 ./ros_setup.sh
 
 #link dotfiles
@@ -13,3 +13,7 @@ ln -sf $PWD/dotfiles/.screenrc ~/.screenrc
 #git setup
 git config --global user.name "DaikiMaekawa"
 git config --global user.email "method_aspect_card@yahoo.co.jp"
+
+#Installing Jekyll with gem
+sudo gem install rdoc
+sudo gem install jekyll
