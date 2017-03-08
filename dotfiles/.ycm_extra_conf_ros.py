0315574493ced864003b895668c1cf1e7722c9ea
DaikiMaekawa <d.maekawa@rapyuta-robotics.com>
2017-03-09 08:47:24 +0900

    re-enable ROS_WORKSPACE env

--- dotfiles/.ycm_extra_conf_ros.py
+++ dotfiles/.ycm_extra_conf_ros.py
@@ -29,8 +29,7 @@ def GetRosIncludePaths():
         return []
     rospack = RosPack()
     includes = []
-    #includes.append(os.path.expandvars('$ROS_WORKSPACE') + '/devel/include')
-    includes.append('~/catkin_ws/devel/include')
+    includes.append(os.path.expandvars('$ROS_WORKSPACE') + '/devel/include')
     for p in rospack.list():
         if os.path.exists(rospack.get_path(p) + '/include'):
             includes.append(rospack.get_path(p) + '/include')
@@ -129,6 +128,7 @@ def GetCompilationDatabaseFolder(filename):
     The compilation_commands.json for the given file is returned by getting
     the package the file belongs to.
     """
+    
     try:
         import rospkg
     except ImportError:
@@ -136,7 +136,7 @@ def GetCompilationDatabaseFolder(filename):
     pkg_name = rospkg.get_package_name(filename)
     if not pkg_name:
         return ''
-    dir = (os.path.expandvars('~/catkin_ws') +
+    dir = (os.path.expandvars('$ROS_WORKSPACE') +
            os.path.sep +
            'build' +
            os.path.sep +
